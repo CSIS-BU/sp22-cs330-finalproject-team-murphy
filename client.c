bda1a5c17dd2be90e3936f2606c965b1941dc1bc
Melvin-W <60675850+Melvin-W@users.noreply.github.com>
2022-04-16 21:17:34 -0500

    Add files via upload

---
+++ client.c
@@ -0,0 +1,60 @@
+#include <stdio.h>	
+#include <string.h>	
+#include <sys/socket.h>	
+#include <arpa/inet.h>	
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+	int socket;
+	struct sockaddr_in serv;
+	char message[1000], server_reply[2000];
+
+	//Creating the socket
+	socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket == -1)
+	{
+		printf("Socket could not be created");
+	}
+	puts("Socket has been made");
+
+	serv.sin_addr.s_addr = inet_addr(""); //address will go here
+	serv.sin_family = AF_INET;
+	serv.sin_port = htons(8888);
+
+	//connecting to the remote server
+	if (connect(socket, (struct sockaddr*)&serv, sizeof(serv)) < 0)
+	{
+		perror("Connection has failed.");
+		return 1;
+	}
+
+	puts("Connection made\n");
+
+	//constantly communicating with server
+	while (1)
+	{
+		printf("Enter message : ");
+		scanf("%s", message);
+
+		//data sending
+		if (send(socket, message, strlen(message), 0) < 0)
+		{
+			puts("message failed to send");
+			return 1;
+		}
+
+		//receiving reply from the serve
+		if (recv(socket, server_reply, 2000, 0) < 0)
+		{
+			puts("recovery of message failed");
+			break;
+		}
+
+		puts("Server reply: ");
+		puts(server_reply);
+	}
+
+	close(socket);
+	return 0;
+}
